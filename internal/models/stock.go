32b50aea514ea78615d6b419d3824910c7ea0d6d
karkki-hub <karkki@worldergroup.com>
2026-03-12 10:09:03 +0530

    errors were corrected

--- internal/models/stock.go
+++ internal/models/stock.go
@@ -1,7 +1,7 @@
 package models
 
 type Stock struct {
-	ID        uint    `json:"id"`
+	ID        uint    `json:"-"`
 	Symbol    string  `json:"symbol"`
 	StockName string  `json:"stock_name"`
 	LastPrice float64 `json:"last_price"`
